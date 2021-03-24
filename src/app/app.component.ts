cbc23d4319f1c0a225b5e27ec523696d4d77e76c
Dominik Pieper <dominik@pieper.io>
2021-03-24 17:08:56 +0100

    Simple ws connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
+import {scan} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'reactive-coffeshop-fe';
+  private socket$: WebSocketSubject<any> = webSocket('ws://localhost:8080/ws/coffee');
+  public coffees$: any;
+
+  ngOnInit(): void {
+    this.coffees$ = this.socket$.pipe(
+      scan((acc: any[], value: any) => [...acc, value], [])
+    );
+  }
 }
